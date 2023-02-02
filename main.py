872eac721551a23645c8b2808edb22586badd916
Jonathan <jonathanbaraldi@gmail.com>
2023-02-02 15:13:27 -0300

    teste

--- main.py
+++ main.py
@@ -78,7 +78,7 @@ for commit in commits:
 
 
         # update
-        repo.update_file(filename, comment.encode(), content, file.sha)
+        repo.update_file(filename, f"ChatGPT's response about `{file.filename}`:\n {response['choices'][0]['text']}", content, file.sha)
         # repo.update_file(filename, "Adding comments to the code, by Jon", modified_content, file.sha)
 
         # repo.create_file(filename+".chatgpt", "Adding comments to the code, by Jon", modified_content, file.sha, "chagpt")
