34708ec39582aef20c570513a0b36d0cc319bf40
Jonathan Baraldi <jonathanbaraldi@Jonathans-Mac-mini.local>
2023-02-03 15:25:56 -0300

    teste

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
 
-# Automated Code Review using the ChatGPT language model
-
 ## Import statements
 import argparse
 import openai
 import os
 from github import Github
-
-# pip3 install GitPython
-
 import base64
   
-
 ## Adding command-line arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('--openai_api_key', help='Your OpenAI API Key')
@@ -30,44 +24,24 @@ openai.api_key = args.openai_api_key
 ## Authenticating with the Github API
 g = Github(args.github_token)
 
-
 ## Selecting the repository
 repo = g.get_repo(os.getenv('GITHUB_REPOSITORY'))
 # repo = g.get_user().get_repo("nodejs-chatgpt")
-
 
 ## Get pull request
 pull_request = repo.get_pull(int(args.github_pr_id))
 
 
-
-
-
-
-
-
-
 ## Loop through the commits in the pull request
 commits = pull_request.get_commits()
-
-
-
-
-
 
 for commit in commits:
     
 
-
     print(commit.sha)
-
-
 
     # Getting the modified files in the commit
     files = commit.files
-
-    
-
 
     for file in files:
 
@@ -87,7 +61,7 @@ for commit in commits:
 
 
         # Comments about the code
-        response1 = openai.Completion.create(
+        response_comment = openai.Completion.create(
             engine=args.openai_engine,
             prompt=("Can you please add some comments to the following code? "+content),
             temperature=float(args.openai_temperature),
@@ -96,7 +70,7 @@ for commit in commits:
 
 
         # Improve the code about security and best practices
-        response2 = openai.Completion.create(
+        response_security_standards = openai.Completion.create(
             engine=args.openai_engine,
             prompt=("Improved and evolve the following code using security and best practices: "+content),
             temperature=float(args.openai_temperature),
@@ -106,13 +80,11 @@ for commit in commits:
 
 
         # Adding a comment to the pull request with ChatGPT's response
-        pull_request.create_issue_comment(f"ChatGPT's comments about `{file.filename}`:\n {response1['choices'][0]['text']}")
+        pull_request.create_issue_comment(f"ChatGPT's comments about `{file.filename}`:\n {response_comment['choices'][0]['text']}")
 
-        pull_request.create_issue_comment(f"ChatGPT's security e best practices about `{file.filename}`:\n {response2['choices'][0]['text']}")
+        pull_request.create_issue_comment(f"ChatGPT's security e best practices about `{file.filename}`:\n {response_security_standards['choices'][0]['text']}")
         # Add the comment to the file
         
-
-
 
         #comment = response['choices'][0]['text']
         #print(comment)
